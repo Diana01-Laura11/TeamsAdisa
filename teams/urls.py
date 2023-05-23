2e07b007d35c0e4477301d5aa09b181b17ba86a1
Diana Laura <dianalauraequihuadiaz@gmail.com>
2023-05-23 13:17:37 -0600

    carga de imagenes

--- teams/urls.py
+++ teams/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 ##Estas importaciones son para poder que django pinte las imagenes 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
@@ -26,6 +27,5 @@ urlpatterns = [
     
 ]
 
-urlpatterns += [
-    re_path(r'⌃media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT,})
-]
+if settings.DEBUG: #new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
